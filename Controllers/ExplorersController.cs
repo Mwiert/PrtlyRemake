5723874e5db082f7838494f0c9eaf7ff2668e835
Mwiert <61664331+Mwiert@users.noreply.github.com>
2022-07-18 11:47:45 +0300

    updates for explores

--- Controllers/ExplorersController.cs
+++ Controllers/ExplorersController.cs
@@ -1,14 +1,37 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Remake.Models;
 
 namespace Remake.Controllers
 {
     [Authorize]
     public class ExplorersController : Controller
     {
+        kesifdbContext db = new kesifdbContext();
+        Kesifler explorers = new Kesifler();
         public IActionResult Index()
         {
-            return View();
+            db.Kesiflers.ToList();
+            db.SaveChanges();
+            return View(db.Kesiflers);
+        }
+        public IActionResult AddExplore(string ExpName)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(ExpName))
+                {
+                    explorers.Ad = ExpName;
+                    db.Kesiflers.Add(explorers);
+                    db.SaveChanges();
+                }  
+            return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
         }
     }
 }
