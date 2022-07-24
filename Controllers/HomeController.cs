86e8e7f0953cfc6863f1b993227e22e4fd10030e
Mwiert <61664331+Mwiert@users.noreply.github.com>
2022-07-25 02:25:12 +0300

    home page updated

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,14 +1,25 @@
 ﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Remake.Models;
 
 namespace Remake.Controllers
 {
     [Authorize]
     public class HomeController : Controller
     {
+        kesifdbContext db = new kesifdbContext();
         public IActionResult Index()
         {
+            List<HomeExportEntites> entitesList = new List<HomeExportEntites>();
+            HomeExportEntites entites = new HomeExportEntites();
+            entites.KesifSayisi = db.Kesiflers.Count().ToString();
+            entites.UrunSayisi= db.Urunlers.Count().ToString();
+            entites.SumUrunAdedi = db.Urunholders.Sum(x => x.UrunAdet).ToString();
+            entites.KategoriSayisi = db.Kategorilers.Count().ToString();
+            entites.KullanıcıSayisi = db.Kullanıcıs.Count().ToString();
+            entitesList.Add(entites);
+            ViewBag.Entities = entitesList;
             return View();
         }
         public async Task<IActionResult> Logout()
---
+++ Models/HomeExportEntites.cs
@@ -0,0 +1,11 @@
+﻿namespace Remake.Models
+{
+    public class HomeExportEntites
+    {
+        public string KesifSayisi { get; set; }
+        public string UrunSayisi { get; set; }
+        public string KategoriSayisi { get; set; }
+        public string KullanıcıSayisi { get; set; }
+        public string SumUrunAdedi { get; set; }
+    }
+}
